d573bd70ee6f1aa4ed1fb8a3be95d5d23c647520
ChadBrewer <chad5972@live.missouristate.edu>
2020-08-19 10:58:16 -0500

    Had to remake the Main Menu due to issues with button functionality. Added the optoins menu and button to get back to the main menu.

---
+++ VRGUI/Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Don't forget to add SceneManagement to load different scenes
+
+public class MenuButtonScript : MonoBehaviour
+{
+
+    //Custom StartGame Function that will start the game when the Start button is clicked
+    public void StartGame()
+    {
+        //Utilize the SceneManager and LoadScene funtion to switch to the Start scene
+        SceneManager.LoadScene("GameStart");
+    }
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
